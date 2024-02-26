c1b66efd7bdcbfea7ccfab71e6243da192c91c27
marbar0407 <145482448+marbar0407@users.noreply.github.com>
2024-02-27 00:04:36 +0300

    геометрия

---
+++ geom.h
@@ -0,0 +1,38 @@
+#ifndef GEOMOCHKA_GEOM_H
+#define GEOMOCHKA_GEOM_H
+
+#include <bits/stdc++.h>
+
+using namespace std;
+#define double long double
+
+class Point {
+public:
+    double x, y;
+    Point(double _x = 0, double _y = 0) : x(_x), y(_y){}
+    Point operator +(Point a) {
+        return {x + a.x, y + a.y};
+    }
+    Point operator -(Point a) {
+        return {x - a.x, y - a.y};
+    }
+    Point turn (double alpha) {
+        return {x * cosl(alpha) - y * sinl(alpha),
+                y * sinl(alpha) - x * cosl(alpha)};
+    }
+    double operator /(Point a) {
+        return (x * a.x + y * a.y);
+    }
+    double operator *(Point a) {
+        return (x * a.y - y * a.x);
+    }
+    double len() {
+        return sqrt(x * x + y * y);
+    }
+};
+
+bool within(Point a, Point b, Point c, Point p) {
+    return (((a - b) * (p - b)) * ((p - b) * (c - b)) >= 0 && ((a - b) * (c - b)) * ((a - b) * (p - b)) >= 0);
+}
+
+#endif //GEOMOCHKA_GEOM_H
